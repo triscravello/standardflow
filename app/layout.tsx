92c05a3ff8116553991c6101b77c8cdeac6ee36d
Tristan Cravello <tlcravello@gmail.com>
2026-03-04 14:18:18 -0500

    Refine metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+//import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
-import Sidebar from "@/components/layout/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,12 +29,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`} bg-gray-50 text-gray-900
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`} bg-gray-50="true" text-gray-900="true"
       >
         <div className="flex flex-col h-screen">
           <Navbar />
           <div className="flex flex-1 overflow-hidden">
-            <Sidebar />
             <main className="flex-1 p-6 overflow-y-auto bg-white">{children}</main>
           </div>
         </div>
