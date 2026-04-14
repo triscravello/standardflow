f1ce3d328d97e4bc5d965ecf79b3bfa1ae1a9bc0
Tristan Cravello <tlcravello@gmail.com>
2026-04-14 14:34:02 -0400

    Added production-ready next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  output: "standalone",
 };
 
 export default nextConfig;
